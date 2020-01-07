13af9ceddc7bb35bc8dbb6db87d74b63a7e5573b
sauron <sauron998@gmail.com>
2020-01-07 17:02:53 +0800

    mssql

--- stores/store.go
+++ stores/store.go
@@ -137,8 +137,7 @@ func getSqlStruct(fields []field) (*sqlStruct, error) {
 		if item.IsPrimary {
 			ss.pcol = item.Col
 			ss.pval = item.Value
-		}
-		if !item.IsEmpty {
+		} else if !item.IsEmpty {
 			ss.cols = append(ss.cols, item.Col)
 			ss.args = append(ss.args, item.Value)
 		}
